cb2a3e071010f31c055a4d006a2e3b2fbda83dea
sharapovaNn <natalya.n.sharapova@gmail.com>
2024-03-28 21:17:53 +0300

    первая функция

---
+++ src/tale.js
@@ -0,0 +1,16 @@
+function kolobok(name) {
+    switch(name) {
+        case 'дедушка':  return 'Я от дедушки ушёл';
+        case 'заяц': return 'Я от зайца ушёл';
+        case 'лиса': return 'Меня съели';
+      }
+  }
+
+  function newYear(name) {
+    switch(name) {
+    case 'Дед Мороз': return `${name}!${name}!${name}!`
+    case 'Снегурочка': return `${name}!${name}!${name}!`
+    }
+  }
+
+  console.log(newYear('Снегурочка'))
